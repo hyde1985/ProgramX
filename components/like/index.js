9c7c3d915d53d892c673acfa9e10736392b8ae9c
Hyde Sun <hyde_sun@sina.com>
2018-08-12 22:13:41 +0800

    code of 6-6

--- components/like/index.js
+++ components/like/index.js
@@ -25,7 +25,13 @@ Component({
    */
   methods: {
     onLike: function(event) {
-      console.log(event)
+      let like = this.properties.like
+      let count = this.properties.count
+      count = like?count-1:count+1
+      this.setData({
+        count:count,
+        like:!like
+      })
     }
   }
 })
